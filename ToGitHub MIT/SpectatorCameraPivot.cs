0110a79ecb9660f58b8599a5f2adb0f0449de000
CreamLeaf <71951376+CreamLeaf@users.noreply.github.com>
2021-01-11 17:46:28 -0500

    Add files via upload

---
+++ ToGitHub MIT/SpectatorCameraPivot.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public class SpectatorCameraPivot : Spatial
+{
+    [Export]
+    public float MoveSpeed = 20;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GD.Print(MoveSpeed);
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        InputEventAction eventAction = new InputEventAction();
+        //eventAction.Action = 
+
+        if (Input.IsActionPressed("Forward"))
+        {
+            GlobalTranslate(new Vector3(0, 0, MoveSpeed));
+        }
+
+        if (Input.IsActionPressed("Back"))
+        {
+            GlobalTranslate(new Vector3(0, 0, MoveSpeed * -1));
+        }
+        base._Input(@event);
+    }
+}
